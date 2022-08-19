3dc75611538937756640cb154c32be38f38d9c36
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-19 14:49:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = Math.max(1,4,7,16,20);
 
 // Write your code here
